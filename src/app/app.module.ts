dcd20ce84952fe4f216246e57dcd595c2f60a161
Xin Li <xinli2@kpmg.com>
2018-06-13 16:13:00 -0600

    added toastr, routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, EventEmitter } from '@angular/core'
 <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    <div [ngSwitch]="event?.time">Time: {{event.time}}
+    <div [ngStyle]="getStartTimeStyle()" [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{event.time}}
       <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
       <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
       <span *ngSwitchDefault>(Normal Start)</span>
@@ -23,6 +23,8 @@ import { Component, Input, EventEmitter } from '@angular/core'
 </div>
   `,
   styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .thumbnail { min-height: 210px; }
     .pad-left: { margin-left: 10px; }
     .well div { color: #bbb; }
@@ -30,7 +32,19 @@ import { Component, Input, EventEmitter } from '@angular/core'
 })
 
 export class EventThumbnailComponent {
-  @Input() event:any  // The Input decorator tells Angular that the event variable will be passed in from another component
+  @Input() event:any;  // The Input decorator tells Angular that the event variable will be passed in from another component
+  getStartTimeClass() {
+    // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    // return {green: isEarlyStart, bold: isEarlyStart};
+    if (this.event && this.event.time === '8:00 am')
+      return 'green bold'; //['green', 'bold']
+    return '';  //[]
+  }
+  getStartTimeStyle():any {
+    if (this.event && this.event.time === '8:00 am')
+      return {color: '#003300', 'font-weight': 'bold'};
+    return {};
+  }
 
   logFoo() {
     console.log('foo');

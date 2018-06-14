427090cc00d4fe4ec368ee71369ca5ab95c69884
Xin Li <xinli2@kpmg.com>
2018-06-14 16:31:18 -0600

    added user profile page (User Module and routing)

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -10,11 +10,15 @@ export const appRoutes: Routes = [
   // this path has to go first, otherwise Angular can't differenticate between events/new and events/:id
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
   // if the URL matches /events, show the EventsListComponent wherever the <router-outlet> component is
-  { path: 'events', component: EventsListComponent, resolve: { events:EventListResolver } },
+  // before resolving this route, call this event list resolver. and when that resolver finishes and returns some data,
+  // add this data to the route as a property named events. So it's going to take the events returned from the resulver,
+  // put that in the property named events in the route
+  { path: 'events', component: EventsListComponent, resolve: {events: EventListResolver} },
   // :id is a parameter placeholder where we expect a value to be passed in on the route
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: '404', component: Error404Component },
   // when the path is empty or when at the root of the site, redirect to /events
-  { path: '', redirectTo: '/events', pathMatch: 'full'}
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
 
 ];
---
+++ src/app/user/user.routes.ts
@@ -0,0 +1,6 @@
+import { ProfileComponent } from './profile.component';
+
+export const userRoutes = [
+  //route is /user/profile
+  { path: 'profile', component: ProfileComponent }
+]
